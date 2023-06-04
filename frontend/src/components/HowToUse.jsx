f9852249cb997f429170c3284df89fd9a39d0fe1
Vedang <vedangvatsal101@gmail.com>
2023-06-04 18:07:54 +0530

    How to Use added

--- frontend/src/components/HowToUse.jsx
+++ frontend/src/components/HowToUse.jsx
@@ -12,10 +12,17 @@ const HowToUse = () => {
       <img src={Line2} alt="" className="howtoline2" />
       <div className="container">
         <div className="content">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque beatae
-          optio dolore est error at blanditiis dignissimos! Illo, cupiditate
-          molestias iure esse natus nesciunt quidem officiis facere quas
-          architecto, dicta, laborum commodi maxime laudantium.
+          1. To record audio from your lecture, click on the microphone icon.{" "}
+          <br />
+          2. Once done recording, click on the icon again to stop recording and
+          download the audio file onto your desktop. <br />
+          3. Upload the audio file and click submit. <br />
+          4. Keywords from the lecture will be used to obtain information
+          relevant to your lecture which will be displayed in the textbox
+          provided. <br />
+          5. Click the button on the bottom right of the box to copy it to your
+          clipboard. To Clear the text box, click on the button at the top left
+          of the box.
         </div>
       </div>
     </div>
